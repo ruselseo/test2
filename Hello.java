306d730ac9ea2a53325bb6155ce6a6312b3e0e84
ruselseo@gmail.com <ruselseo@gmail.com>
2022-11-02 00:52:05 -0700

    ADD function implemented

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(add(5, 6));
     }
+
+     public static int add(int a, int b){
+        return a+b;
+     }
+
 }
